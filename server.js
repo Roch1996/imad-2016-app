1c1709fc7cef667ce2b7123e5fb6f62ace059f87
Roch1996 <chetanbedarekar12@gmail.com>
2016-11-01 22:38:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/Converted_file_8d20a587.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Converted_file_8d20a587.png'));
 });
 
 
